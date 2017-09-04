9bdf081c3bf73f7f188a286d9dd9cae4ceb7f9c6
Indra Octama <omyank2007i@gmail.com>
2017-09-04 14:30:53 +0700

    First Time commit

---
+++ js/form-scripts.js
@@ -0,0 +1,35 @@
+/**
+ * Created by indraoctama on 9/4/17.
+ */
+
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+
+    $.ajax({
+        type: "POST",
+        url: "php/process.php",
+        data: "name=" + name + "&email=" + email + "&message=" + message,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            }
+        }
+    });
+}
+function formSuccess(){
+    $( "#msgSubmit" ).removeClass( "hidden" );
+}
+
+
+$("#contactForm").validator().on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitForm();
+    }
+});
